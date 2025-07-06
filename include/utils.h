ef8ea842fe1b1766f55006cdbadbd6a835641223
clarkchenc <clarkchenc@qq.com>
2025-07-06 14:55:58 +0800

    up

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,8 @@
+#pragma once
+
 #include <chrono>
 
-int64_t time_cost(const std::chrono::steady_clock::time_point& st,
-                  const std::chrono::steady_clock::time_point& en) {
+inline int64_t time_cost(const std::chrono::steady_clock::time_point& st,
+                         const std::chrono::steady_clock::time_point& en) {
   return std::chrono::duration_cast<std::chrono::nanoseconds>(en - st).count();
 }
