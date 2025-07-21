ae737b442cb6fed707dd5cb8db408afc1fb87b65
clarkchenc <clarkchenc@qq.com>
2025-07-21 16:11:42 +0800

    opt pq

--- include/pq_dis.cc
+++ include/pq_dis.cc
@@ -5,16 +5,17 @@
 
 #include <xmmintrin.h>
 #include <pmmintrin.h>
+#include <tmmintrin.h>  // SSE3 指令集
 
 #include "utils.h"
 
-int horizontal_add_epi32(__m128i v) const {
+int horizontal_add_epi32(__m128i v) {
   __m128i sum = _mm_hadd_epi32(v, v);  // [x0+x1, x2+x3, x0+x1, x2+x3]
   sum = _mm_hadd_epi32(sum, sum);      // [x0+x1+x2+x3, ...]
   return _mm_cvtsi128_si32(sum);
 }
 
-uint16_t get_pq_dis(const void* p_vec1, const void* p_vec2, size_t subspace_num, size_t cluster_num) const {
+uint16_t get_pq_dis(const void* p_vec1, const void* p_vec2, size_t subspace_num, size_t cluster_num) {
   uint16_t dis = 0;
 
   uint16_t* ptr_vec1 = (uint16_t*)p_vec1;
@@ -26,13 +27,13 @@ uint16_t get_pq_dis(const void* p_vec1, const void* p_vec2, size_t subspace_num,
   __m128i tmp;
   for (size_t i = 0; i < subspace_num; i += 8) {
     v1 = _mm_set_epi32(ptr_vec1[ptr_vec2[0]], ptr_vec1[1 * cluster_num + ptr_vec2[1]],
-                       ptr_vec1[2 * cluster_num_ + ptr_vec2[2]], ptr_vec1[3 * cluster_num_ + ptr_vec2[3]]);
-    v2 = _mm_set_epi32(ptr_vec1[4 * cluster_num_ + ptr_vec2[4]], ptr_vec1[5 * cluster_num_ + ptr_vec2[5]],
-                       ptr_vec1[6 * cluster_num_ + ptr_vec2[6]], ptr_vec1[7 * cluster_num_ + ptr_vec2[7]]);
+                       ptr_vec1[2 * cluster_num + ptr_vec2[2]], ptr_vec1[3 * cluster_num + ptr_vec2[3]]);
+    v2 = _mm_set_epi32(ptr_vec1[4 * cluster_num + ptr_vec2[4]], ptr_vec1[5 * cluster_num + ptr_vec2[5]],
+                       ptr_vec1[6 * cluster_num + ptr_vec2[6]], ptr_vec1[7 * cluster_num + ptr_vec2[7]]);
 
     tmp = _mm_add_epi32(v1, v2);
     sum = _mm_add_epi32(sum, tmp);
-    ptr_vec1 += 8 * cluster_num_;
+    ptr_vec1 += 8 * cluster_num;
     ptr_vec2 += 8;
   }
   dis = horizontal_add_epi32(sum);
